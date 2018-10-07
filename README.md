0c541638a1278d60d2f7bf3371d6dbec2d1afc3e
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2018-10-06 22:52:38 -0400

    (add) Append AMPA processed data

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DS320
 Anti-Microbial Peptide Classification Benchmark Utility
+
+## Setup
+To run the programs within the code base, you must have Python3
+
+## File Structure
---
+++ src/util/merge_csv.py
@@ -0,0 +1,8 @@
+'''
+Merge CSV Utility
+Merges CSV and Validates Record Against Original - Reports Missing Records
+
+Author: Yuya Jeremy Ong (yjo5006@psu.edu)
+'''
+from __future__ import print_function
+import os
